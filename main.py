8470eb2bb0af7619a7f94b05b94d084a60dff5ab
riaz-khan-16 <goriazmte@gmail.com>
2025-07-01 11:32:11 +0600

    testing

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ def add(a,b):
     print(a+b)
     return a+b
 
+def subtraction(a,b):
+    return (a-b)
--- test_add.py
+++ test_add.py
@@ -1,6 +1,9 @@
-from test import add
+from main import add, subtraction
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+def test_sub():
+    assert subtraction(2,3)==-1
+    
